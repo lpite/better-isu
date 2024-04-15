dff482f6afad6307e0b44f01157ad8497c9c7fda
lpite <farionsasa@gmail.com>
2024-04-15 19:38:10 +0300

    fix sheet size

--- @/components/schedule-carousel.tsx
+++ @/components/schedule-carousel.tsx
@@ -129,7 +129,7 @@ export default function ScheduleCarousel() {
 						<path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
 					</svg>
 				</SheetTrigger>
-				<SheetContent side="bottom">
+				<SheetContent side="bottom" className="max-h-96 overflow-y-auto">
 					<SheetHeader className="mb-4">
 						<SheetTitle>Предмети які показувати в розкладі</SheetTitle>
 					</SheetHeader>

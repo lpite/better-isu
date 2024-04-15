fc7b62eb56d5590372a83bd60c1fd53bd738c949
lpite <farionsasa@gmail.com>
2024-04-15 19:40:11 +0300

    do not use batch requests

--- trpc/trpc-client.ts
+++ trpc/trpc-client.ts
@@ -1,4 +1,4 @@
-import { httpBatchLink } from '@trpc/client';
+import { httpBatchLink, httpLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '../trpc/router';
 
@@ -7,19 +7,12 @@ export const trpc = createTRPCNext<AppRouter>({
 	config(opts) {
 		return {
 			links: [
-				httpBatchLink({
+				httpLink({
 					/**
 					 * If you want to use SSR, you need to use the server's full URL
 					 * @link https://trpc.io/docs/v11/ssr
 					 **/
 					url: `/api/trpc`,
-					
-					// You can pass any HTTP headers you wish here
-					async headers() {
-						return {
-							// authorization: getAuthCookie(),
-						};
-					},
 				}),
 			],
 		};

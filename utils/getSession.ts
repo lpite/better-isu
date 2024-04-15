ed96ba64bd4d333c60d127d73e19793a4955b6be
lpite <farionsasa@gmail.com>
2024-04-15 19:40:45 +0300

    fix multiple session updates kinda

--- utils/getSession.ts
+++ utils/getSession.ts
@@ -3,8 +3,7 @@ import { db } from "./db";
 import { Session } from "../types/session";
 import { decryptText } from "./encryption";
 import { sql } from "kysely";
-import { getProfilePage, getSchedulePage, getSubjectsPage } from "./getPage";
-import { parseScheduleTable } from "./parseScheduleTable";
+import { getProfilePage, getSubjectsPage } from "./getPage";
 import getScheduleByApi from "./getScheduleByApi";
 
 export default async function getSession(req: NextApiRequest): Promise<{
@@ -29,6 +28,7 @@ export default async function getSession(req: NextApiRequest): Promise<{
 			data: null
 		}
 	}
+
 
 
 	const now = new Date().getTime() - (new Date().getTimezoneOffset() * 60);
@@ -58,44 +58,61 @@ export default async function getSession(req: NextApiRequest): Promise<{
 
 
 async function refreshSession(session: Session) {
-	console.log("refreshing session")
+	try {
+		console.log("refreshing session")
 
-	const user = await db.selectFrom("user")
-		.selectAll()
-		.where("id", "=", session.user_id)
-		.executeTakeFirst()
+		const user = await db.selectFrom("user")
+			.selectAll()
+			.where("id", "=", session.user_id)
+			.executeTakeFirst()
 
-	if (!user) {
-		console.error("can't refresh session");
-		return null
-	}
+		if (!user) {
+			console.error("can't refresh session no user");
+			return null
+		}
+		await db.updateTable("session")
+			.set({
+				created_at: sql`now()`
+			})
+			.where("id", "=", session.id)
+			.executeTakeFirst()
 
-	const credentials = JSON.parse(decryptText(user.credentials, process.env.ENCRYPTION_KEY || ""));
+		const credentials = JSON.parse(decryptText(user.credentials, process.env.ENCRYPTION_KEY || ""));
 
-	const formData = new FormData()
-	formData.append("login", credentials.login);
-	formData.append("passwd", credentials.password);
-	formData.append("btnSubmit", "%D3%E2%B3%E9%F2%E8");
+		const formData = new FormData()
+		formData.append("login", credentials.login);
+		formData.append("passwd", credentials.password);
+		formData.append("btnSubmit", "%D3%E2%B3%E9%F2%E8");
 
-	const res = await fetch("https://isu1.khmnu.edu.ua/isu/dbsupport/logon.php", {
-		method: "POST",
-		body: formData,
-		cache: "no-cache",
-		credentials: "include",
-		redirect: "manual"
-	})
-
-	const cookie = res.headers.getSetCookie().toString().split("=")[1].split(";")[0];
-
-	const newSession = await db.updateTable("session")
-		.set({
-			isu_cookie: cookie,
-			created_at: sql`now()`
+		const res = await fetch("https://isu1.khmnu.edu.ua/isu/dbsupport/logon.php", {
+			method: "POST",
+			body: formData,
+			cache: "no-cache",
+			credentials: "include",
+			redirect: "manual"
 		})
-		.where("id", "=", session.id)
-		.returningAll()
-		.executeTakeFirst()
-	return newSession;
+
+		const cookie = res.headers.getSetCookie().toString().split("=")[1].split(";")[0];
+
+		const newSession = await db.updateTable("session")
+			.set({
+				isu_cookie: cookie,
+				created_at: sql`now()`
+			})
+			.where("id", "=", session.id)
+			.returningAll()
+			.executeTakeFirstOrThrow()
+		return newSession;
+	} catch (err) {
+		console.error(err);
+		await db.updateTable("session")
+			.set({
+				created_at: sql` now() - interval '1 hour'`
+			})
+			.where("id", "=", session.id)
+			.executeTakeFirst()
+	}
+	
 
 }
 

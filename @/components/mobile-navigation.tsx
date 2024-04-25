65d38a7be663d9ba7b9e9568e492c39ec68cf905
lpite <farionsasa@gmail.com>
2024-04-25 22:08:22 +0300

    proper light theme support

--- @/components/mobile-navigation.tsx
+++ @/components/mobile-navigation.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 export default function MobileNavigation() {
 	const router = useRouter();
 	return (
-		<nav className="fixed bottom-0 left-0 w-full h-14 bg-slate-900 flex items-center pb-2">
+		<nav className="fixed bottom-0 left-0 w-full h-14 dark:bg-slate-900 bg-slate-100 flex items-center pb-2">
 			<Link href="/">
 				<a className="flex grow justify-center">
 
@@ -14,7 +14,7 @@ export default function MobileNavigation() {
 						viewBox="0 0 24 24" 
 						strokeWidth={1.5} 
 						stroke="currentColor" 
-						className={`w-8 h-8 ${router.pathname === "/" ? " text-slate-200" : " text-slate-400"}`}
+						className={`w-8 h-8 ${router.pathname === "/" ? " dark:text-slate-200" : " text-slate-400"}`}
 					>
 						<path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
 					</svg>
@@ -28,7 +28,7 @@ export default function MobileNavigation() {
 						viewBox="0 0 24 24" 
 						strokeWidth={1.5} 
 						stroke="currentColor" 
-						className={`w-8 h-8 ${router.pathname.startsWith("/profile") ? " text-slate-200" : " text-slate-400"}`}
+						className={`w-8 h-8 ${router.pathname.startsWith("/profile") ? " dark:text-slate-200" : " text-slate-400"}`}
 					>
 						<path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
 					</svg>
@@ -42,7 +42,7 @@ export default function MobileNavigation() {
 						viewBox="0 0 24 24" 
 						strokeWidth={1.5} 
 						stroke="currentColor" 
-						className={`w-8 h-8 ${router.pathname.startsWith("/about") ? " text-slate-200" : " text-slate-400"}`}
+						className={`w-8 h-8 ${router.pathname.startsWith("/about") ? " dark:text-slate-200" : " text-slate-400"}`}
 
 					>
 						<path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
--- pages/about.tsx
+++ pages/about.tsx
@@ -6,27 +6,27 @@ export default function AboutPage() {
 	return (
 		<main className="flex flex-col items-center py-4 px-4 pb-14">
 			<h2 className="text-2xl mb-6">Технології</h2>
-			<div className="bg-slate-900 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
+			<div className="dark:dark:bg-slate-900 border-2 border-2 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
 				<span className="text-xl mb-2">Typescript</span>
 				<span className="text-md">Як javascript тільки краще</span>
 			</div>
-			<div className="bg-slate-900 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
+			<div className="dark:bg-slate-900 border-2 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
 				<span className="text-xl mb-2">nextjs</span>
 				<span className="text-md">Вебфреймворк оснований на react</span>
 			</div>
-			<div className="bg-slate-900 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
+			<div className="dark:bg-slate-900 border-2 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
 				<span className="text-xl mb-2">tailwindcss</span>
 				<span className="text-md">CSS фреймворк для простої стилізації</span>
 			</div>
-			<div className="bg-slate-900 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
+			<div className="dark:bg-slate-900 border-2 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
 				<span className="text-xl mb-2">PostgreSQL</span>
 				<span className="text-md">Швиденька та потужна база даних</span>
 			</div>
-			<div className="bg-slate-900 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
+			<div className="dark:bg-slate-900 border-2 w-full py-6 px-4 rounded-xl flex flex-col mb-3">
 				<span className="text-xl mb-2">trpc</span>
 				<span className="text-md">Зберігає типізацію між бекендом та фронтендом</span>
 			</div>
-			<div className="border-2 w-full py-6 px-4 rounded-xl flex flex-col mb-3 wiggle_bg">
+			<div className="dark:bg-black border-2 w-full py-6 px-4 rounded-xl flex flex-col mb-3 wiggle_bg">
 				<Link href="https://github.com/lpite/better-isu">
 					<a className="text-xl" target="_blank">GitHub репозиторій</a>
 				</Link>

c0a4c514e55de62a11e3a70d88d4726bb2317d46
lpite <farionsasa@gmail.com>
2024-05-29 23:59:00 +0300

    fix infinity useEffect calls

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,7 +105,7 @@ export default function Home() {
           </>
         ))}
 
-        <ScheduleCarousel subjects={subjects} />
+        <ScheduleCarousel subjects={subjects || []} />
       </div>
       <MobileNavigation />
     </main>

e63f8dc5fc2a2981e10a53e7a3efcfe01de5279d
lpite <farionsasa@gmail.com>
2024-06-04 09:31:48 +0300

    scroll journal name

--- pages/journal.tsx
+++ pages/journal.tsx
@@ -155,13 +155,13 @@ export default function JournalPage({ days, month, journalName }: { days: Day[],
 		<>
 			<header className="p-2 flex">
 				<Link href="/">
-					<a className="p-2">
+					<a className="px-1">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
 							<path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 						</svg>
 					</a>
 				</Link>
-				<span className="text-xl">{journalName}</span>
+				<span className={`text-xl text-nowrap overflow-x-auto`}>{journalName}</span>
 			</header>
 			<main className="gap-1 p-2">
 				{month.map((m) => {

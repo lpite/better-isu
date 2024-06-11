ba5eff1e02dacbc1f5f7038f50bd05a9a25bbf4d
lpite <farionsasa@gmail.com>
2024-06-11 21:29:22 +0300

    better journal controls

--- pages/journal.tsx
+++ pages/journal.tsx
@@ -11,15 +11,7 @@ export default function JournalPage() {
 	if (isLoading) {
 		return (
 			<>
-				<header className="px-1 pt-3 pb-2 flex items-center border-b">
-					<Link href="/">
-						<a className="pl-2 pr-4">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-							</svg>
-						</a>
-					</Link>
-				</header>
+				<header className="px-1 pt-3 pb-2 flex items-center border-b"></header>
 				<main className="h-full flex items-center justify-center">
 					<div className="flex w-full h-64 items-center justify-center">
 						<div className="border-2 border-blue-600 p-4 rounded-full border-b-transparent animate-spin"></div>
@@ -31,15 +23,7 @@ export default function JournalPage() {
 	if (!data || isError) {
 		return (
 			<>
-				<header className="px-1 pt-3 pb-2 flex items-center border-b">
-					<Link href="/">
-						<a className="pl-2 pr-4">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-							</svg>
-						</a>
-					</Link>
-				</header>
+				<header className="px-1 pt-3 pb-2 flex items-center border-b"></header>
 				<main className="gap-1 p-2 flex items-center justify-center">
 					<h1>Щось пішло не так :(</h1>
 				</main>
@@ -49,17 +33,10 @@ export default function JournalPage() {
 	const { months, journalName } = data
 	return (
 		<>
-			<header className="px-1 pt-3 pb-2 flex items-center border-b">
-				<Link href="/">
-					<a className="pl-2 pr-4">
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-							<path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-						</svg>
-					</a>
-				</Link>
+			<header className="px-3 pt-3 pb-2 flex items-center border-b fixed top-0 left-0 right-0" style={{ backgroundColor: "#020817" }}>
 				<h1 className={`text-xl text-nowrap overflow-x-auto`}>{journalName}</h1>
 			</header>
-			<main className="gap-1 p-2">
+			<main className="gap-1 p-2 pt-12 pb-14">
 				{months.map((m) => {
 					const gradesForMonth = m.grades 
 						
@@ -74,7 +51,17 @@ export default function JournalPage() {
 						</Fragment>
 					)
 				})}
-		
+				<div className="fixed bottom-0 left-0 right-0 flex items-center justify-center h-12 bg-slate-800">
+					<Link href="/">
+						<a className="pl-2 pr-4">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 inline mr-3">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+							</svg>
+							<span className="inline">Повернутися</span>
+						</a>
+					</Link>
+				</div>
+				
 			</main>
 		</>
 	)

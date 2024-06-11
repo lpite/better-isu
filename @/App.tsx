d0cdeec2337abc92089a7fffb5c3675cb0852535
lpite <farionsasa@gmail.com>
2024-06-11 21:01:14 +0300

    init posthog only on production

--- @/App.tsx
+++ @/App.tsx
@@ -5,13 +5,16 @@ import posthog from "posthog-js"
 import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined') {
-	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
-		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+	if (process.env.NEXT_PUBLIC_POSTHOG_KEY && process.env.NODE_ENV !== 'development') {
+		posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
+			api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
     
-		loaded: (posthog) => {
-			if (process.env.NODE_ENV === 'development') posthog.debug()
-		},
-	})
+			loaded: (posthog) => {
+				if (process.env.NODE_ENV === 'development') posthog.debug()
+			},
+		})
+	}
+	
 }
 
 export default function App({ children }: any) {

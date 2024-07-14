0ddcce21afebad0fa03a3b984667b53eb5a35eaa
lpite <farionsasa@gmail.com>
2024-07-14 19:46:35 +0300

    remove posthog

--- @/App.tsx
+++ @/App.tsx
@@ -1,30 +1,12 @@
 import NextProgress from "next-progress";
 import ServiceWorkerUpdater from "./components/service-worker-updater";
 
-import posthog from "posthog-js"
-import { PostHogProvider } from 'posthog-js/react'
-
-if (typeof window !== 'undefined') {
-	if (process.env.NEXT_PUBLIC_POSTHOG_KEY && process.env.NODE_ENV !== 'development') {
-		posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
-			api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-    
-			loaded: (posthog) => {
-				if (process.env.NODE_ENV === 'development') posthog.debug()
-			},
-		})
-	}
-	
-}
-
 export default function App({ children }: any) {
 	return (
 		<>
 			<ServiceWorkerUpdater />
 			<NextProgress delay={200} options={{ showSpinner: false }} />
-			<PostHogProvider client={posthog}>
-				{children}
-			</PostHogProvider>
+			{children}
 		</>
 	)
 }

bf3e2846aa89e4f16fa20066ab372d0ac21be6b8
lpite <farionsasa@gmail.com>
2024-07-15 22:33:30 +0300

    prettify types

--- types/session.ts
+++ types/session.ts
@@ -2,9 +2,8 @@ import { Selectable } from "kysely";
 import { DB } from "./db";
 
 export type Session = Omit<Selectable<DB["session"]>, "isu_cookie"> & {
-	isu_cookie?: string
-}
-
+  isu_cookie?: string;
+};
 
 //Для клієнта не повертаю куку університету
 export type ClientSession = Omit<Session, "isu_cookie">;

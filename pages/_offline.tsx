3dda5f33910b3202da8bcfd83b66d046365b7ffa
lpite <farionsasa@gmail.com>
2024-09-21 20:30:46 +0300

    fix css

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,8 +1,8 @@
 export default function OfflinePage() {
   return (
     <main className="flex flex-col items-center justify-center w-full h-full">
-      <h1 className="text-6xl">йоой</h1>
-      <h2 className="text-2xl">
+      <h1 className="text-6xl text-center">йоой</h1>
+      <h2 className="text-2xl text-center">
         Можливо у вас немає інтернету або сервер хну помер)
       </h2>
     </main>

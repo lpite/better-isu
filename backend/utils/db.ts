d32b52b0b927a057fb201b899339829e58cf1022
lpite <farionsasa@gmail.com>
2024-10-31 09:29:19 +0200

    return to using pg for db connection

--- backend/utils/db.ts
+++ backend/utils/db.ts
@@ -1,17 +1,15 @@
-import { Kysely } from "kysely";
-import { PostgresJSDialect } from "kysely-postgres-js";
-
+import { Kysely, PostgresDialect } from "kysely";
+import pg from "pg";
 import { DB } from "../types/db";
-import postgres from "postgres";
 
 export const db = new Kysely<DB>({
-  dialect: new PostgresJSDialect({
-    postgres: postgres({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      ssl: true,
+    dialect: new PostgresDialect({
+      pool: new pg.Pool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        ssl: true,
+      }),
     }),
-  }),
-});
+  });

5a880ba018d40f74b365ebc63c1e3d03e260102d
lpite <farionsasa@gmail.com>
2025-03-03 09:29:22 +0200

    remove stupid fix

--- api/[[...route]].js
+++ api/[[...route]].js
@@ -264,7 +264,7 @@ module.exports = async (req, res) => {
     }
     const groups = await getGroups(faculty.facultyId, course);
     const group = groups.find((el) => el.groupName === groupName);
-    group.currSem = group.currSem + 1;
+    group.currSem = group.currSem;
     return res.send(JSON.stringify(group));
   }
 

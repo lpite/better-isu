0cf7c659bfb05be1b10a15fc4229f7cf45a48ba9
lpite <farionsasa@gmail.com>
2025-04-13 13:00:50 +0300

    ihateit

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,13 +1,9 @@
 import { getProfilePage } from "@/data/getProfilePage";
 import { useAppStore } from "@/stores/useAppStore";
-import { laggy } from "@/utils/laggySwr";
 import useSWR from "swr";
 
 export function useProfile() {
   const { session } = useAppStore();
   return useSWR("profile", () => getProfilePage(session?.token || ""), {
-    revalidateOnFocus: false,
-    revalidateIfStale: false,
-    use: [laggy],
   });
 }

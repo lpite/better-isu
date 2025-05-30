3d6f1be1671345ad4f9e282e42aa9cb660b21070
lpite <farionsasa@gmail.com>
2025-05-31 00:21:47 +0300

    temp fix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export let API_URL = "";
 
-if (process.env.NODE_ENV === "development") {
   API_URL = "https://4fw4nhik6tvkvjbnbvrqxcwpoe0jqhlp.lambda-url.eu-central-1.on.aws";
+if (process.env.NODE_ENV === "development") {
 }

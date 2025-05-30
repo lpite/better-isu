9d9c3acadb9bd8612546da36aa0b0a4d154c7667
lpite <farionsasa@gmail.com>
2025-05-30 20:49:41 +0300

    and change url not working :)

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export let API_URL = "";
 
 if (process.env.NODE_ENV === "development") {
-  API_URL = "http://localhost:3001";
+  API_URL = "https://444jm7vuswlze34rhplvied5ky0bpwlv.lambda-url.eu-central-1.on.aws";
 }

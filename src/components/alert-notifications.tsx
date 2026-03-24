e32c2327aa7595d997796384ca037957a77c35b5
lpite <farionsasa@gmail.com>
2026-03-25 01:57:08 +0200

    dark color scheme for alrt

--- src/components/alert-notifications.tsx
+++ src/components/alert-notifications.tsx
@@ -8,10 +8,10 @@ type AlertNotificationProps = {
 export function AlertNotification({ text, show }: AlertNotificationProps) {
   return createPortal(
     <div
-      className={`fixed ${show ? "top-4" : "-top-20"} duration-300 ease-in-out start-1/2 -translate-x-1/2 w-full max-w-64 p-2 bg-yellow-200 flex items-center border rounded-xl`}
+      className={`fixed ${show ? "top-4" : "-top-20"} duration-300 ease-in-out start-1/2 -translate-x-1/2 w-full max-w-64 p-2 bg-yellow-200 dark:bg-yellow-400 flex items-center border rounded-xl`}
     >
       <div className="size-6 mx-2 border-2 border-blue-700 border-t-transparent animate-spin rounded-full inline-block shrink-0"></div>
-      {text}
+      <span className="dark:text-slate-800">{text}</span>
     </div>,
     document.body,
   );
